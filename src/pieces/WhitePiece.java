1641e2863fcb628b6e28c3497346adf6f3599476
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-18 16:12:07 +0800

    removed the implements Piece since Abstract Class "CheckersPiece" already implements it.

--- src/pieces/WhitePiece.java
+++ src/pieces/WhitePiece.java
@@ -1,6 +1,6 @@
 package pieces;
 
-public class WhitePiece extends CheckersPiece implements Piece{
+public class WhitePiece extends CheckersPiece{
     public WhitePiece(int row, int col, boolean isKing) {
         super(row, col, isKing);
         System.out.println(this.row + " " + this.col);
