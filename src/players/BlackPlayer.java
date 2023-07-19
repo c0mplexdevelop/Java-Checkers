f5b1da94628001d223aff8d1a2908ec39c22546d
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-19 20:01:09 +0800

    Added move functions that take into account piece promotions and possible directions

--- src/players/BlackPlayer.java
+++ src/players/BlackPlayer.java
@@ -1,4 +1,6 @@
 package players;
+import exceptions.IllegalMoveException;
+import pieces.CheckersPiece;
 import pieces.Piece;
 
 public class BlackPlayer implements Player {
@@ -13,8 +15,61 @@ public class BlackPlayer implements Player {
     }
 
     @Override
-    public void move(Piece piece, int row, int col) {
+    public void move(Piece piece, String direction) throws IllegalMoveException {
+        int pieceRow = piece.getRow();
+        int pieceCol = piece.getCol();
+        boolean pieceIsKing = piece.isKing();
 
+        int deltaRow = 0;
+        int deltaCol = 0;
+
+        if(!pieceIsKing) {
+            switch(direction.toLowerCase()) {
+                case "down-left" -> {
+                    deltaRow = -1;
+                    deltaCol = -1;
+                }
+                case "down-right" -> {
+                    deltaRow = -1;
+                    deltaCol = 1;
+                }
+
+                case "up-left", "up-right" -> {
+                    throw new IllegalMoveException("Piece is not promoted yet!");
+                }
+
+                default -> {
+                    throw new IllegalMoveException("Invalid direction!");
+                }
+            }
+        } else {
+            switch(direction.toLowerCase()) {
+                case "down-left" -> {
+                    deltaRow = -1;
+                    deltaCol = -1;
+                }
+                case "down-right" -> {
+                    deltaRow = -1;
+                    deltaCol = 1;
+                }
+                case "up-left" -> {
+                    deltaRow = 1;
+                    deltaCol = -1;
+                }
+                case "up-right" -> {
+                    deltaRow = 1;
+                    deltaCol = 1;
+                }
+            }
+        }
+
+
+        if(pieceRow + deltaRow < 0 || pieceCol + deltaCol < 0 || pieceRow + deltaRow > 7 || pieceCol + deltaCol > 7) {
+            throw new IllegalMoveException("Piece cannot move out of bounds!");
+        }
+
+        piece.setRow(pieceRow + deltaRow); // Plus since if its negative, it will be subtracted anyways
+        piece.setCol(pieceCol + deltaCol);
     }
 
     public Piece getPiece(Piece[][] board, int row, int col) {
--- src/players/WhitePlayer.java
+++ src/players/WhitePlayer.java
@@ -1,5 +1,6 @@
 package players;
 
+import exceptions.IllegalMoveException;
 import pieces.Piece;
 
 public class WhitePlayer implements Player {
@@ -16,8 +17,62 @@ public class WhitePlayer implements Player {
     }
 
     @Override
-    public void move(Piece piece, String direction) {
+    public void move(Piece piece, String direction) throws IllegalMoveException {
+        int pieceRow = piece.getRow();
+        int pieceCol = piece.getCol();
+        boolean pieceIsKing = piece.isKing();
 
+        int deltaRow = 0;
+        int deltaCol = 0;
+
+        if(!pieceIsKing) {
+            switch(direction.toLowerCase()) {
+                case "up-left" -> {
+                    deltaRow = 1;
+                    deltaCol = -1;
+                }
+                case "up-right" -> {
+                    deltaRow = 1;
+                    deltaCol = 1;
+                }
+                case "down-left", "down-right" -> {
+                    throw new IllegalMoveException("Piece is not promoted yet!");
+                }
+                default -> {
+                    throw new IllegalMoveException("Invalid direction!");
+                }
+            }
+        } else {
+            switch(direction.toLowerCase()) {
+                case "up-left" -> {
+                    deltaRow = 1;
+                    deltaCol = -1;
+                }
+                case "up-right" -> {
+                    deltaRow = 1;
+                    deltaCol = 1;
+                }
+                case "down-left" -> {
+                    deltaRow = -1;
+                    deltaCol = -1;
+                }
+                case "down-right" -> {
+                    deltaRow = -1;
+                    deltaCol = 1;
+                }
+                default -> {
+                    throw new IllegalMoveException("Invalid direction!");
+                }
+            }
+        }
+
+
+        if(pieceRow + deltaRow < 0 || pieceCol + deltaCol < 0 || pieceRow + deltaRow > 7 || pieceCol + deltaCol > 7) {
+            throw new IllegalMoveException("Piece cannot move out of bounds!");
+        }
+
+        piece.setRow(pieceRow + deltaRow); // Plus since if its negative, it will be subtracted anyways
+        piece.setCol(pieceCol + deltaCol);
     }
 
     public Piece getPiece(Piece[][] board, int row, int col) {
