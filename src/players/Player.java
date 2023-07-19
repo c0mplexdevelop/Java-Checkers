1a46344979ea046b0885b713346a4fb56d32c587
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-19 20:01:40 +0800

    Player now throws a Illegal Move Exception.

--- src/players/Player.java
+++ src/players/Player.java
@@ -1,9 +1,10 @@
 package players;
+import exceptions.IllegalMoveException;
 import pieces.Piece;
 
 public interface Player {
     String getName();
-    void move(Piece piece, String direction);
+    void move(Piece piece, String direction) throws IllegalMoveException;
 
     Piece getPiece(Piece[][] board, int row, int col);
     boolean capture(Piece piece, Piece capturingPiece);
