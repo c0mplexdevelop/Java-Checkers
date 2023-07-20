2bd4846637bdd16f35fae144b21bd97dc9cf0d77
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-20 18:00:37 +0800

    Interface now throws an IllegalPositionException

--- src/players/Player.java
+++ src/players/Player.java
@@ -1,11 +1,13 @@
 package players;
 import exceptions.IllegalMoveException;
+import exceptions.IllegalPositionException;
 import pieces.Piece;
 
 public interface Player {
     String getName();
     void move(Piece piece, String direction) throws IllegalMoveException;
 
-    Piece getPiece(Piece[][] board, int row, int col);
+    Piece getPiece(Piece[][] board, int row, int col) throws IllegalPositionException;
+
     boolean capture(Piece piece, Piece capturingPiece);
 }
