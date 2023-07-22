f3c1eb964ad4544cbd73ea9a58e6b500e3b1fc1e
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-22 23:06:13 +0800

    Updated to return a CheckersPiece object with the appropriate type via the PieceType enum

--- src/factories/BlackPieceFactory.java
+++ src/factories/BlackPieceFactory.java
@@ -1,12 +1,13 @@
 package factories;
 
+import pieces.CheckersPiece;
 import pieces.Piece;
-import pieces.BlackPiece;
+import pieces.PieceType;
 
 public class BlackPieceFactory implements PieceFactory {
     @Override
     public Piece createPiece(int row, int col, boolean isKing) {
 
-        return new BlackPiece(row, col, isKing);
+        return new CheckersPiece(row, col, isKing, PieceType.BLACK);
     }
 }
--- src/factories/WhitePieceFactory.java
+++ src/factories/WhitePieceFactory.java
@@ -1,10 +1,12 @@
 package factories;
 
+import pieces.CheckersPiece;
 import pieces.Piece;
-import pieces.WhitePiece;
+import pieces.PieceType;
+
 public class WhitePieceFactory implements PieceFactory {
     @Override
     public Piece createPiece(int row, int col, boolean isKing) {
-        return new WhitePiece(row, col, isKing);
+        return new CheckersPiece(row, col, isKing, PieceType.WHITE);
     }
 }
