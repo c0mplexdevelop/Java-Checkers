7ca87b19eb7e2f904e2c263f4e2c900239530c64
c0mplexdevelop <snakebigbosslel@gmail.com>
2023-07-22 23:05:12 +0800

    Removed Black and WhitePiece since its redundant. CheckersPiece.java now has a enum "PieceType"

--- src/pieces/BlackPiece.java
+++
@@ -1,8 +0,0 @@
-package pieces;
-
-public class BlackPiece extends CheckersPiece{
-    public BlackPiece(int row, int col, boolean isKing) {
-        super(row, col, isKing);
-        System.out.println(this.row + " " + this.col);
-    }
-}
--- src/pieces/WhitePiece.java
+++
@@ -1,8 +0,0 @@
-package pieces;
-
-public class WhitePiece extends CheckersPiece{
-    public WhitePiece(int row, int col, boolean isKing) {
-        super(row, col, isKing);
-        System.out.println(this.row + " " + this.col);
-    }
-}
